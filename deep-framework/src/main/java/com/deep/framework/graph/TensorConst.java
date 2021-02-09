14a6107f560a3367fdc4f6d77bc24fb2a95dc145
sense <351695627@qq.com>
2021-02-09 11:54:53 +0800

    优化

--- deep-framework/src/main/java/com/deep/framework/graph/TensorConst.java
+++ deep-framework/src/main/java/com/deep/framework/graph/TensorConst.java
@@ -1,8 +1,6 @@
 package com.deep.framework.graph;
 
-import java.io.Serializable;
-
-public class TensorConst extends Tensor implements Serializable {
+public class TensorConst extends Tensor {
 
     public TensorConst(double input) { super(input); }
 
