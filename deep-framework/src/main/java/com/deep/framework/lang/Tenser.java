68b051bf7a9944104e0bc6b171b6274f754d645b
sense <351695627@qq.com>
2021-08-22 09:47:10 +0800

    优化

--- deep-framework/src/main/java/com/deep/framework/lang/Tenser.java
+++ deep-framework/src/main/java/com/deep/framework/lang/Tenser.java
@@ -68,7 +68,7 @@ public class Tenser<T> {
         return next;
     }
 
-    public int[] getLength() {
+     int[] getLength() {
         int next = 1;
         int[] length = new int[shape.length - 1];
         for (int i = length.length; 0 < i; i--) {
