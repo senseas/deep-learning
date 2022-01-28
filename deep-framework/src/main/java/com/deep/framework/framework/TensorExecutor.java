8c6c4811fe4587d8dd9d1d6c60d5e9ada9fb9812
Admin <351695627@qq.com>
2022-01-28 10:53:49 +0800

    优化

--- deep-framework/src/main/java/com/deep/framework/framework/TensorExecutor.java
+++ deep-framework/src/main/java/com/deep/framework/framework/TensorExecutor.java
@@ -2,7 +2,6 @@ package com.deep.framework.framework;
 
 import com.deep.framework.graph.None;
 import com.deep.framework.graph.Tensor;
-import com.deep.framework.lang.StopWatch;
 import com.deep.framework.lang.Tensers;
 import com.deep.framework.lang.function.Func2;
 import lombok.Data;
@@ -34,13 +33,9 @@ public class TensorExecutor<E> implements Serializable {
     }
 
     public void run() {
-        StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
         tensor.forward();
         this.backward();
         tensor.reduce();
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint());
     }
 
     public void forward(E input, E label) {
