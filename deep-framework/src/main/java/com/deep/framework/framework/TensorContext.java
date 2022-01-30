c1de62006b1cb725529493179ea30dc437e4f58d
ChengSense <351695627@qq.com>
2022-01-30 09:01:58 +0800

    优化

--- deep-framework/src/main/java/com/deep/framework/framework/TensorContext.java
+++ deep-framework/src/main/java/com/deep/framework/framework/TensorContext.java
@@ -8,109 +8,195 @@ import com.jogamp.opencl.*;
 import java.io.Serializable;
 import java.nio.Buffer;
 import java.nio.DoubleBuffer;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.Objects;
 
 import static com.jogamp.opencl.CLMemory.Mem.READ_WRITE;
 
 public class TensorContext implements Serializable {
 
     private final Tensor tensor;
+
     private final CLContext context;
+
     private final CLCommandQueue queue;
+
     private final CLKernel compute, gradient;
-    private List<CLBuffer<DoubleBuffer>> input;
-    private CLBuffer<DoubleBuffer> output;
+
+    private CLBuffer<DoubleBuffer> valueBuffer, gradBuffer;
+
     private Block block;
 
     public TensorContext(Tensor tensor, CLContext context, CLCommandQueue queue, CLKernel compute, CLKernel gradient) {
+
         this.tensor = tensor;
+
         this.context = context;
+
         this.queue = queue;
+
         this.compute = compute;
+
         this.gradient = gradient;
+
     }
 
     private void setComputeArgs(final Object... params) {
 
-        for (Tensor tensor : tensor.getInput()) {
-            CLBuffer buffer = getBuffer((double[]) tensor.getValue());
+        Arrays.stream(tensor.getInput()).forEach(o -> {
+
+            CLBuffer buffer = o.getContext().getValueBuffer();
+
             queue.putWriteBuffer(buffer, true);
+
             compute.putArg(buffer);
-        }
 
-        output = getBuffer((double[]) tensor.getValue());
-        queue.putWriteBuffer(output, true);
-        compute.putArg(output);
+        });
 
-        for (Object param : params) {
-            setObjectConvert(compute, param);
-        }
+        CLBuffer buffer = getValueBuffer();
+
+        queue.putWriteBuffer(buffer, true);
+
+        compute.putArg(buffer);
+
+        Arrays.stream(params).forEach(o -> setObjectConvert(compute, o));
 
         compute.rewind();
     }
 
     public void compute(final Object... params) {
+
         setComputeArgs(params);
+
         queue.put2DRangeKernel(compute, 0, 0, block.x, block.y, 0, 0);
-        queue.putReadBuffer(output, true);
-        DoubleBuffer buffer = output.getBuffer();
-        double[] value = (double[]) tensor.getValue();
-        IntStream.range(0, value.length).forEach(i -> value[i] = buffer.get(i));
-        output.release();
+
+        queue.putReadBuffer(getValueBuffer(), true);
+
+        DoubleBuffer buffer = getValueBuffer().getBuffer();
+
+        buffer.get((double[]) tensor.getValue());
+
+        Arrays.stream(tensor.getInput()).forEach(tensor -> tensor.getContext().release());
+
+        release();
+
     }
 
     private void setGradientArgs(final Object... params) {
-        input = new ArrayList();
 
-        for (Tensor tensor : tensor.getInput()) {
-            CLBuffer valueBuffer = getBuffer((double[]) tensor.getValue());
-            queue.putWriteBuffer(valueBuffer, true);
-            gradient.putArg(valueBuffer);
+        Arrays.stream(tensor.getInput()).forEach(o -> {
 
-            CLBuffer gradBuffer = getBuffer((double[]) tensor.getGrad());
-            queue.putWriteBuffer(gradBuffer, true);
-            gradient.putArg(gradBuffer);
-            input.add(gradBuffer);
-        }
+            CLBuffer buffer = o.getContext().getValueBuffer();
 
-        output = getBuffer((double[]) tensor.getGrad());
-        queue.putWriteBuffer(output, true);
-        gradient.putArg(output);
+            queue.putWriteBuffer(buffer, true);
 
-        for (Object param : params) {
-            setObjectConvert(gradient, param);
-        }
+            gradient.putArg(buffer);
+
+        });
+
+
+        Arrays.stream(tensor.getInput()).forEach(o -> {
+
+            CLBuffer buffer = o.getContext().getGradBuffer((double[]) o.getGrad());
+
+            queue.putWriteBuffer(buffer, true);
+
+            gradient.putArg(buffer);
+
+        });
+
+        CLBuffer buffer = getGradBuffer();
+
+        queue.putWriteBuffer(buffer, true);
+
+        gradient.putArg(buffer);
+
+        Arrays.stream(params).forEach(param -> setObjectConvert(gradient, param));
 
         gradient.rewind();
+
     }
 
     public void gradient(final Object... params) {
+
         setGradientArgs(params);
+
         queue.put2DRangeKernel(gradient, 0, 0, block.x, block.y, 0, 0);
-        IntStream.range(0, input.size()).forEach(l -> {
-            CLBuffer<DoubleBuffer> clBuffer = input.get(l);
+
+        Arrays.stream(tensor.getInput()).forEach(o -> {
+
+            CLBuffer<DoubleBuffer> clBuffer = o.getContext().getGradBuffer();
+
             queue.putReadBuffer(clBuffer, true);
+
             DoubleBuffer buffer = clBuffer.getBuffer();
-            Tensor in = tensor.getInput()[l];
-            double[] value = (double[]) in.getValue();
-            IntStream.range(0, value.length).forEach(i -> value[i] = buffer.get(i));
+
+            buffer.get((double[]) o.getGrad());
+
         });
-        output.release();
+
+        Arrays.stream(tensor.getInput()).forEach(tensor -> tensor.getContext().release());
+
+        release();
+
     }
 
-    public <T> CLBuffer getBuffer(double[] arr) {
+    public CLBuffer getBuffer(double[] arr) {
+
         Buffer directBuffer = Buffers.newDirectDoubleBuffer(arr);
+
         CLBuffer<Buffer> buffer = context.createBuffer(directBuffer, READ_WRITE);
+
         buffer.getBuffer().position(0);
+
         return buffer;
+
+    }
+
+    public CLBuffer<DoubleBuffer> getValueBuffer() {
+
+        if (Objects.nonNull(valueBuffer)) return valueBuffer;
+
+        return valueBuffer = getBuffer((double[]) tensor.getValue());
+
+    }
+
+    public CLBuffer<DoubleBuffer> getGradBuffer(double... data) {
+
+        if (Objects.nonNull(gradBuffer)) return gradBuffer;
+
+        return gradBuffer = getBuffer((double[]) tensor.getGrad());
+
+    }
+
+    public void release() {
+
+        if (Objects.nonNull(valueBuffer)) {
+
+            valueBuffer.release();
+
+            valueBuffer = null;
+
+        }
+
+        if (Objects.nonNull(gradBuffer)) {
+
+            gradBuffer.release();
+
+            gradBuffer = null;
+
+        }
+
     }
 
     public TensorContext setBlock(int... x) {
-        if (x.length == 1) this.block = new Block(x[0]);
-        if (x.length == 2) this.block = new Block(x[0], x[1]);
-        if (x.length == 3) this.block = new Block(x[0], x[1], x[2]);
+        if (x.length == 1) {
+            this.block = new Block(x[0]);
+        } else if (x.length == 2) {
+            this.block = new Block(x[0], x[1]);
+        } else if (x.length == 3) {
+            this.block = new Block(x[0], x[1], x[2]);
+        }
         return this;
     }
 
