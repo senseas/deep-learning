0e03f6fd25b3aa4594b82b544668e149e1b08273
Admin <351695627@qq.com>
2022-10-30 14:26:05 +0800

    优化

--- deep-framework/src/test/java/com/deep/framework/AppxTest.java
+++ deep-framework/src/test/java/com/deep/framework/AppxTest.java
@@ -8,10 +8,27 @@ import org.junit.Test;
 public class AppxTest {
 
     @Test
-    public void squarexTest() {
+    public void softmaxTest() {
         TensorFlow tf = new TensorFlow();
-        Tensor softmax = tf.softmaxCrossx(new Tensor(new int[]{2}), new Tensor(new int[]{2}));
+        Tensor softmax = tf.softmax(new Tensor(new int[]{3}));
         TensorExecutor executor = new TensorExecutor(softmax);
+        executor.run();
+    }
+
+    @Test
+    public void sigmoidxTest() {
+        TensorFlow tf = new TensorFlow();
+        Tensor sigmoidx = tf.sigmoidx(new Tensor(new int[]{3}));
+        TensorExecutor executor = new TensorExecutor(sigmoidx);
+        executor.run();
+    }
+
+    @Test
+    public void softmaxCrossxTest() {
+        TensorFlow tf = new TensorFlow();
+        Tensor softmaxCrossx = tf.softmaxCrossx(new Tensor(new int[]{5}),new Tensor(new int[]{5}));
+        TensorExecutor executor = new TensorExecutor(softmaxCrossx);
+        executor.run();
         executor.run();
     }
 
