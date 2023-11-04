eca80db8c1e5a59f43d9f31bece1b72955157307
ChengSense <351695627@qq.com>
2023-11-04 18:49:03 +0800

    优化

--- deep-framework/src/main/java/com/deep/framework/cudnn/CudnnConfig.java
+++ deep-framework/src/main/java/com/deep/framework/cudnn/CudnnConfig.java
@@ -9,14 +9,10 @@ import static jcuda.runtime.JCuda.cudaStreamCreate;
 
 public class CudnnConfig {
     public static final cudnnHandle handle;
-    public static final cudaStream_t stream;
 
     static {
-        // Initialize cudnn library
         JCudnn.setExceptionsEnabled(true);
         handle = new cudnnHandle();
         cudnnCreate(handle);
-        stream = new cudaStream_t();
-        cudaStreamCreate(stream);
     }
 }
